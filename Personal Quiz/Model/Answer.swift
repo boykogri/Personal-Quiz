4370781cdb66bf31c8d3516ee6dc2d34af9811ca
Григорий Бойко <boiko.gri@gmail.com>
2020-07-12 10:59:37 +0700

    Create models

---
+++ Personal Quiz/Model/Answer.swift
@@ -0,0 +1,11 @@
+//
+//  Answer.swift
+//  Personal Quiz
+//
+//  Created by Григорий Бойко on 11/07/2020.
+//  Copyright © 2020 Grigory Boyko. All rights reserved.
+//
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
---
+++ Personal Quiz/Model/ResponseType.swift
@@ -0,0 +1,13 @@
+//
+//  ResponseType.swift
+//  Personal Quiz
+//
+//  Created by Григорий Бойко on 11/07/2020.
+//  Copyright © 2020 Grigory Boyko. All rights reserved.
+//
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
